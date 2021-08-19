c48fbbd4e8470372a07a0a0837ca1801482393dd
verifsec <49231682+verifsec@users.noreply.github.com>
2021-08-19 15:21:55 +0900

    d

--- fuzz/fuzzer0.c
+++ fuzz/fuzzer0.c
@@ -7,8 +7,9 @@
 int hello(uint8_t* buf);
 
 int LLVMFuzzerTestOneInput(uint8_t* data, size_t size) {
+	if (size < 3) return 0;
 	uint8_t* buf = (uint8_t*)malloc(3);	
-	if (read(data, buf, 3) < 1) {
+	if (memcpy(buf, data, 3) < 1) {
 		printf("Hum?\n");
 	} else {	
 		hello(buf);
